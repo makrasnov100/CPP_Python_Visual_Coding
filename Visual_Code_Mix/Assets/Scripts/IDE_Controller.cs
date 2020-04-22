85d756e4cdc2eb9a6a4eece461fa4ae0ede2cf16
Kostiantyn Makrasnov <32151128+makrasnov100@users.noreply.github.com>
2020-04-21 19:05:26 -0700

    Stted on node type selection

---
+++ Visual_Code_Mix/Assets/Scripts/IDE_Controller.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class IDE_Controller : MonoBehaviour
+{
+    static IDE_Controller instance;
+
+
+    //Input settings
+    public float timeForDoubleClick = 0.2f;
+
+
+
+    //Instance variables
+    public int currentClicks = 0;
+
+    //Used prefabs/objketcs
+    public GameObject genericNode;
+    public Camera mainCamera;
+
+
+    //Node map
+    Dictionary<string, NodeIdentity> nodes = new Dictionary<string, NodeIdentity>();
+
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+            instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(doubleClickCooldown());
+        }
+    }
+
+    IEnumerator doubleClickCooldown()
+    {
+        currentClicks += 1;
+        if (currentClicks >= 2)
+        {
+            currentClicks = 0;
+            createNewNode();
+            StopAllCoroutines();
+        }
+        else 
+        {
+            yield return new WaitForSeconds(timeForDoubleClick);
+            currentClicks -= 1;
+        }
+    }
+
+
+
+    //[NODE RELATED]
+    void createNewNode()
+    {
+        GameObject curNode = Instantiate(genericNode, mainCamera.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity, gameObject.transform);
+        NodeIdentity curIdentity = curNode.AddComponent<NodeIdentity>();
+        curNode.transform.position = new Vector3(curNode.transform.position.x, curNode.transform.position.y, 0);
+        string nodeId = randomString(8);
+        while (nodes.ContainsKey(nodeId))
+        {
+            nodeId = randomString(8);
+        }
+        curIdentity.setupNode(nodeId);
+
+        nodes.Add(nodeId, curIdentity);
+    }
+
+    public string randomString(int length)
+    {
+        var newWord = new char[length];
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        for (int i = 0; i < length; i++)
+        {
+            newWord[i] = chars[UnityEngine.Random.Range(0, chars.Length)];
+        }
+        return new string(newWord);
+    }
+}
---
+++ Visual_Code_Mix/Assets/Scripts/NodeIdentity.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeType{none,data,selection,loop,function}
+public enum DataType {none, intT, doubleT,stringT,boolT}
+public enum FunctionType {none, cpp, python, visual}
+public class NodeIdentity : MonoBehaviour
+{
+
+
+    //Node settings;
+    string id = "";
+    NodeType nodeType = NodeType.none;
+    DataType dataType = DataType.none;
+    FunctionType funcType = FunctionType.none;
+
+    //NODE SETUP
+    public void setupNode(string id)
+    {
+        this.id = id;
+        beginSetup();
+    }
+
+
+    void beginSetup()
+    {
+        //Display type of node selection
+        displaySelectionOptions(
+            new List<string>() {"Data", "Selection", "Loop", "Function"},
+            new List<Color>() { }
+        );
+    }
+
+    public void onSelectedNodeType(NodeType typeSelected)
+    {
+        nodeType = typeSelected;
+        //Display type of node selection
+        displaySelectionOptions(
+            new List<string>() { "Data", "Selection", "Loop", "Function" },
+            new List<Color>() {Color.magenta, Color.cyan, Color.yellow, Color.green}
+        );
+    }
+
+    public void onSelectedDataType(DataType typeSelected)
+    {
+        dataType = typeSelected;
+        clearSelections();
+    }
+
+
+    //[UI RELATED]
+    float optionPadding = .2f;
+    float outsidePadding = .2f;
+    List<NodeOptionSetup> nodeOptions = new List<NodeOptionSetup>();
+    public GameObject containerRow;
+    public GameObject nodeOption;
+    void displaySelectionOptions(List<string> text, List<Color> colors)
+    {
+        //Check if can display
+        if (text.Count != colors.Count)
+        {
+            return;
+        }
+
+        //Calculate the dimensions of options
+        float parentWidth = gameObject.transform.localScale.x;
+        float optionWidth = (parentWidth - (2 * outsidePadding) - (optionPadding * (text.Count-1))) / text.Count;
+        float parentHeight = gameObject.transform.localScale.y;
+        float optionHeight = parentHeight - (2 * outsidePadding);
+
+        //Populate the Node
+        for (int i = 0; i < text.Count; i++)
+        {
+            //Instnatiete
+        }
+    }
+
+    void clearSelections()
+    {
+        for (int i = 0; i < nodeOptions.Count; i++)
+        {
+            Destroy(nodeOptions[i]);
+        }
+        nodeOptions.Clear();
+    }
+}
