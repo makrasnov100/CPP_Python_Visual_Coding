49f42febf731c9910ed383d540ca8ca9827d9c8a
Kostiantyn Makrasnov <32151128+makrasnov100@users.noreply.github.com>
2020-05-13 22:37:59 -0700

    output UI complete

---
+++ Visual_Code_Mix/Assets/OutputConsoleController.cs
@@ -0,0 +1,51 @@
+﻿using Shapes2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutputConsoleController : MonoBehaviour
+{
+
+    //UI References
+    GameObject allOutputUI;
+    GameObject messageHolder;
+    GameObject messagePrefab;
+
+    //Instance Variables
+    List<GameObject> messages = new List<GameObject>();
+
+
+    //Message Methods
+    void AddOutput(string msg, OutputMessageType type)
+    {
+        //Create message add to list of other outputs
+        GameObject curMsgGO = Instantiate(messagePrefab);
+        curMsgGO.transform.SetParent(messageHolder.transform);
+
+        //Setup the message text and apearence
+        OutputMessageIdentity curMsg = curMsgGO.GetComponent<OutputMessageIdentity>();
+        curMsg.SetupMessage(msg, type);
+
+        messages.Add(curMsgGO);
+    }
+
+    void ClearOutput()
+    {
+        foreach (GameObject msg in messages)
+        {
+            Destroy(msg);
+        }
+        messages.Clear();
+    }
+
+    void OpenOutput()
+    {
+        allOutputUI.SetActive(true);
+    }
+
+    void CloseOutput()
+    {
+        allOutputUI.SetActive(false);
+    }
+
+}
---
+++ Visual_Code_Mix/Assets/OutputMessageIdentity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum OutputMessageType {info, warning, error}
+
+public class OutputMessageIdentity : MonoBehaviour
+{
+    //UI references
+    public TMP_Text uiHandle;
+
+    public void SetupMessage(string msg, OutputMessageType type)
+    {
+        //set text
+        uiHandle.text = msg;
+
+        //set color
+        if (type == OutputMessageType.warning)
+            uiHandle.color = Color.yellow;
+        else if (type == OutputMessageType.error)
+            uiHandle.color = Color.red;
+        else
+            uiHandle.color = Color.green;
+
+        //reset scale?
+
+    }
+}
